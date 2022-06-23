aa83144c5c2322ac7ac55eedfd61a1d3832eb3ad
elvstejd <elvistejeda0910@gmail.com>
2022-06-23 17:10:20 -0400

    Log body

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,18 @@ const db = require('../db');
 /* GET home page. */
 router.post('/', async function (req, res, next) {
   const estudiantes = req.body;
+  console.log(estudiantes);
 
   let valuesToInsert = '';
   estudiantes.forEach((estudiante, index) => {
     valuesToInsert += `('${estudiante.nombre}', '${estudiante.apellido}', '${estudiante.email}', '${estudiante.telefono}', ${estudiante.monto}, '${estudiante.producto}')${index === estudiantes.length - 1 ? ';' : ', '}`
   });
 
-  const query = await db.query(
+  await db.query(
     "insert into financiamiento_estudiantil (nombre, apellido, email, telefono, monto, producto) values " + valuesToInsert
   );
 
-  console.log(query);
+  console.log(`${estudiantes.length} nuevos registros añadidos a la base de datos.`);
   res.sendStatus(200)
 });
 
